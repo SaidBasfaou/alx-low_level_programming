ebe981300f771c7a7d259d31e920f0c9d8f85c92
SaidBasfaou <said.bsf.99@gmail.com>
2023-03-17 09:41:01 -0700

    print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,22 +14,22 @@ int main(void)
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 1 ; j <= 9 ; j++)
+		{
+			for (k = 2 ; k <= 9 ; k++)
 			{
-				for (k = 2 ; k <= 9 ; k++)
+				if (i < j && j < k)
 				{
-					if (i < j && j < k)
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
 					{
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(k + '0');
-						if (i != 7 || j != 8 || k != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
